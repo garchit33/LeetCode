c6405c3830aae89e106e52c840034b0538ef5fbc
Archit Garg <garchit33@gmail.com>
2024-10-17 15:39:55 +0530

    Time: 0 ms (100%), Space: 43.1 MB (67.13%) - LeetHub

--- 1185-find-in-mountain-array/1185-find-in-mountain-array.java
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.java
@@ -2,58 +2,53 @@
  * // This is MountainArray's API interface.
  * // You should not implement it, or speculate about its implementation
  * interface MountainArray {
- * public int get(int index) {}
- * public int length() {}
+ *     public int get(int index) {}
+ *     public int length() {}
  * }
  */
-
+ 
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
         int peakElement = peakElement(mountainArr);
-        if (target == mountainArr.get(peakElement)) {
+        if(target == mountainArr.get(peakElement))
             return peakElement;
-        }
-        int index = orderAgnosticBS(mountainArr, target, 0, peakElement);
-        if (index != -1) {
+
+        int index = orderBasedBS(0, peakElement, target, mountainArr);
+        if(index != -1)
             return index;
-        }
-        return orderAgnosticBS(mountainArr, target, peakElement + 1, mountainArr.length() - 1);
+            
+        return orderBasedBS(peakElement+1, mountainArr.length()-1, target, mountainArr);
     }
 
-    int peakElement(MountainArray mountainArr) {
+    int peakElement(MountainArray mountainArr){
         int start = 0;
-        int end = mountainArr.length() - 1;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+        int end = mountainArr.length()-1;
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(mountainArr.get(mid) < mountainArr.get(mid+1))
                 start = mid + 1;
-            } else {
+            else 
                 end = mid;
-            }
         }
         return start;
     }
 
-    int orderAgnosticBS(MountainArray mountainArr, int target, int start, int end) {
+    int orderBasedBS(int start, int end, int target, MountainArray mountainArr){
         boolean isAsc = mountainArr.get(start) < mountainArr.get(end);
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (target == mountainArr.get(mid)) {
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(target == mountainArr.get(mid))
                 return mid;
-            }
-            if (isAsc) {
-                if (target < mountainArr.get(mid)) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            } else {
-                if (target > mountainArr.get(mid)) {
-                    end = mid - 1;
-                } else {
-                     start = mid + 1;
-                }
-            }
+            if(isAsc)
+                if(target < mountainArr.get(mid))
+                    end = mid-1;
+                else
+                    start = mid+1;
+            else
+                if(target > mountainArr.get(mid))
+                    end = mid-1;
+                else
+                    start = mid+1;
         }
         return -1;
     }
