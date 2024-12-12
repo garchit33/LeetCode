4341f313eded4d00afbc4da864974eb0b64f56c4
Archit Garg <garchit33@gmail.com>
2024-12-12 21:53:13 +0530

    Time: 0 ms (100%), Space: 41.7 MB (5.36%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -12,28 +12,27 @@ class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode root = head;
         if(head == null)
-            return root;
+            return null;
 
         ListNode initialHead = new ListNode(-1);
         int pos = 1;
         while(pos < left){
-            initialHead = head; //Prev Node to Node from where the LinkedList needs to be reversed
+            initialHead = head;
             head = head.next;
             pos++;
         }
-
-        ListNode temp = head; //Node from where the LinkedList needs to be reversed
-        ListNode[] arr = reverse(head, pos, right);
-        initialHead.next = arr[0]; //Prev Node to Node from where the LinkedList needs to be reversed will point to Prev returned by reverse function
-        temp.next = arr[1]; //Node from where the LinkedList needs to be reversed will point to head returned by reverse function
+        ListNode temp = head;
+        ListNode[] arr = reverseLinkedList(head, pos, right);
+        initialHead.next = arr[0];
+        temp.next = arr[1];
 
         if(left == 1)
-            return initialHead.next; //if linkedlist need to be reversed from 1st position then return prev from reverse function
+            return initialHead.next;
 
         return root;
     }
 
-    private ListNode[] reverse(ListNode head, int pos, int right){
+    private ListNode[] reverseLinkedList(ListNode head, int pos, int right){
         ListNode prev = null;
         while(pos <= right){
             ListNode temp = head.next;
@@ -43,6 +42,6 @@ class Solution {
 
             pos++;
         }
-        return new ListNode[]{prev, head};
+        return new ListNode[]{prev,head};
     }
 }
