679b28defc16661b921cac5e01bdb6c56b86e519
Archit Garg <garchit33@gmail.com>
2024-12-12 23:04:29 +0530

    Time: 1 ms (90.44%), Space: 43.4 MB (92.46%) - LeetHub

--- 0655-print-binary-tree/0655-print-binary-tree.java
+++ 0655-print-binary-tree/0655-print-binary-tree.java
@@ -14,25 +14,27 @@
  * }
  */
 class Solution {
-    class Pair {
+    class Pair{
         int row;
         int col;
         TreeNode node;
         Pair(int row, int col, TreeNode node){
-            this.row=row;
-            this.col=col;
-            this.node=node;
+            this.row = row;
+            this.col = col;
+            this.node = node;
         }
     }
     public List<List<String>> printTree(TreeNode root) {
-        int height = depth(root)-1;
-        int totalCol = (int)Math.pow(2, height+1)-1;
-
         List<List<String>> ans = new ArrayList<>();
-        Queue<Pair> queue = new LinkedList<>();
-        queue.offer(new Pair(0, totalCol/2, root));
+        if(root == null)
+            return ans;
 
-        while(queue.size() > 0){
+        int height = depth(root)-1;
+        int totalCol = (int) Math.pow(2, height+1)-1;
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(0,totalCol/2,root));
+        while(queue.size()>0){
             int size = queue.size();
             List<String> list = new ArrayList<>();
             for(int i=0; i<totalCol; i++){
@@ -42,12 +44,12 @@ class Solution {
                 Pair rm = queue.poll();
                 int r = rm.row;
                 int c = rm.col;
-                list.set(c, String.valueOf(rm.node.val));
+                list.set(c,String.valueOf(rm.node.val));
 
-                if(rm.node.left!=null)
-                    queue.offer(new Pair(r+1, c-(int)Math.pow(2, height-r-1), rm.node.left));
-                if(rm.node.right!=null)
-                    queue.offer(new Pair(r+1, c+(int)Math.pow(2, height-r-1), rm.node.right));
+                if(rm.node.left != null)
+                    queue.offer(new Pair(r+1, c-(int) Math.pow(2,height-r-1), rm.node.left));
+                if(rm.node.right != null)
+                    queue.offer(new Pair(r+1, c+(int) Math.pow(2,height-r-1), rm.node.right));
             }
             ans.add(list);
         }
@@ -57,9 +59,8 @@ class Solution {
     private int depth(TreeNode root){
         if(root == null)
             return 0;
-        
         int left = depth(root.left);
         int right = depth(root.right);
-        return Math.max(left, right)+1;
+        return Math.max(left,right)+1;
     }
 }
