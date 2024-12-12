cc76b10cfee6020cd0184e90f1efcdd1cbfc7140
Archit Garg <garchit33@gmail.com>
2024-12-13 00:17:50 +0530

    Time: 2 ms (53.49%), Space: 45.2 MB (12.79%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.java
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -15,28 +15,29 @@
  */
 class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return buildTree (nums, 0, nums.length-1);
+        return buildTree(nums, 0, nums.length-1);
     }
 
     private TreeNode buildTree(int[] nums, int begin, int end){
         if(begin>end)
             return null;
-        int maxIdx = getMax(nums,begin,end);
-        TreeNode node = new TreeNode(nums[maxIdx]);
-        node.left = buildTree(nums,begin,maxIdx-1);
-        node.right = buildTree(nums, maxIdx+1, end);
+        
+        int max = getMaxIdx(nums, begin, end);
+        TreeNode node = new TreeNode(nums[max]);
+        node.left = buildTree(nums, begin, max-1);
+        node.right = buildTree(nums, max+1, end);
         return node;
     }
 
-    private int getMax(int[] nums, int l, int r){
-        int max = nums[l];
-        int idx = l;
-        while(l<=r){
-            if(nums[l] > max){
-                max = nums[l];
-                idx = l;
+    private int getMaxIdx(int[] nums, int left, int right){
+        int max = nums[left];
+        int idx = left;
+        while(left <= right){
+            if(nums[left] > max){
+                max = nums[left];
+                idx = left;
             }
-            l++;
+            left++;
         }
         return idx;
     }
