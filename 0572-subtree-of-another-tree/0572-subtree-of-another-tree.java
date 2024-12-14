add4ed57109f04bea3df5fbce291047416632c49
Archit Garg <garchit33@gmail.com>
2024-12-14 13:06:38 +0530

    Time: 2 ms (97.45%), Space: 44.6 MB (18.58%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -21,24 +21,23 @@ class Solution {
             return false;
         if(subRoot == null)
             return false;
-        
+
         if(root.val == subRoot.val)
-            if(check(root, subRoot))
+            if(check(root,subRoot))
                 return true;
-        
+
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
     private boolean check(TreeNode root, TreeNode subRoot){
         if(root == null && subRoot == null)
             return true;
-        if(root == null)
-            return false;
-        if(subRoot == null)
+        if(root == null || subRoot == null)
             return false;
 
         if(root.val != subRoot.val)
             return false;
+        
         return check(root.left, subRoot.left) && check(root.right, subRoot.right);
     }
 }
