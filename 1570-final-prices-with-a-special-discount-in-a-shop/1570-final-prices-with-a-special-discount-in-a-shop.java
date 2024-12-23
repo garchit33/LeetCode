95370fd382b5bef9ffcba92d1668e219994d1901
Archit Garg <garchit33@gmail.com>
2024-12-23 19:55:02 +0530

    Time: 2 ms (48.81%), Space: 43.6 MB (68.63%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        //int[] arr = new int[prices.length];
         for(int i=0; i<prices.length; i++){
-            // boolean flag = false;
             int x=i+1;
             while(x<prices.length){
                 if(x<prices.length && prices[x] <= prices[i]){
                     prices[i] = prices[i]-prices[x];
-                    // flag = true;
                     break;
                 }
                 x++;
             }
-            // if(!flag)
-            //     arr[i] = prices[i];
         }
         return prices;
     }
