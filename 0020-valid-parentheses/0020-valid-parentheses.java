1211e22687e0890fb32f45fbf0faf1a7bfc17d2a
Archit Garg <garchit33@gmail.com>
2025-02-05 00:45:51 +0530

    Time: 2 ms (97.64%), Space: 41.9 MB (48.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,19 +4,19 @@ class Solution {
         for(char c : s.toCharArray()){
             if(c == '(' || c == '[' || c == '{'){
                 stack.push(c);
-            }else if(stack.size() == 0){
+            }else if(stack.size()<=0){
                 return false;
             }else {
-                if(c == ')' && stack.peek() != '(')
+                if(c == ')' && stack.peek()!='(')
                     return false;
-                if(c == ']' && stack.peek() != '[')
+                if(c == ']' && stack.peek()!='[')
                     return false;
-                if(c == '}' && stack.peek() != '{')
+                if(c == '}' && stack.peek()!='{')
                     return false;
-                
+
                 stack.pop();
             }
         }
-        return stack.size() == 0;
+        return stack.size()==0;
     }
 }
