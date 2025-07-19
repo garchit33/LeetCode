0efdebc557758a5934c6cff556f9ef827690597e
Archit Garg <garchit33@gmail.com>
2025-07-19 17:17:59 +0530

    Time: 1 ms (80.25%), Space: 41.7 MB (49.18%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String arr1[] = version1.split("\\.");
+        String arr2[] = version2.split("\\.");
+        int i=0, j=0;
+        while(i<arr1.length && j<arr2.length){
+            int a = Integer.parseInt(arr1[i]);
+            int b = Integer.parseInt(arr2[j]);
+            if(a<b)
+                return -1;
+            if(a>b)
+                return 1;
+            i++;
+            j++;
+        }
+        while(i<arr1.length){
+            int a = Integer.parseInt(arr1[i++]);
+            if(a>0)
+                return 1;
+        }
+        while(j<arr2.length){
+            int a = Integer.parseInt(arr2[j++]);
+            if(a>0)
+                return -1;
+        }
+        return 0;
+    }
+}
