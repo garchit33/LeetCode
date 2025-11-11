881fb3301f02e2a23c7bfd4e6aa2e1204cfa3467
Archit Garg <garchit33@gmail.com>
2025-11-11 21:41:56 +0530

    Time: 2 ms (52.49%), Space: 46.4 MB (5.29%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.java
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        boolean[] vis = new boolean[rooms.size()];
+        boolean vis[] = new boolean[rooms.size()];
+
         Queue<Integer> q = new LinkedList<>();
         q.offer(0);
-        vis[0]=true;
-        while(q.size()>0){
+        vis[0] = true;
+        while(q.size() > 0){
             int rm = q.poll();
             for(int nbr : rooms.get(rm)){
                 if(!vis[nbr]){
