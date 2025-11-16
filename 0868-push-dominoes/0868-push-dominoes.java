602d4ffd52d3475af00dfd5df92700061ccf7bf0
Archit Garg <garchit33@gmail.com>
2025-11-16 13:42:34 +0530

    Time: 26 ms (38.01%), Space: 48.2 MB (9.93%) - LeetHub

---
+++ 0868-push-dominoes/0868-push-dominoes.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String pushDominoes(String dominoes) {
+        char[] arr = dominoes.toCharArray();
+        int n = dominoes.length();
+        int[] forces = new int[n];
+
+        int force = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] == 'R'){
+                force = n;
+            }else if(arr[i] == 'L'){
+                force = 0;
+            }else {
+                force = Math.max(force-1, 0);
+            }
+            forces[i] += force;
+        }
+
+        force = 0;
+        for(int i = n-1; i>=0; i--){
+            if(arr[i] == 'L'){
+                force = n;
+            }else if(arr[i] == 'R'){
+                force = 0;
+            }else {
+                force = Math.max(force-1, 0);
+            }
+
+            forces[i] -= force;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int f : forces){
+            if(f>0){
+                sb.append("R");
+            }else if(f<0){
+                sb.append("L");
+            }else {
+                sb.append(".");
+            }
+        }
+
+        return sb.toString();
+    }
+}
