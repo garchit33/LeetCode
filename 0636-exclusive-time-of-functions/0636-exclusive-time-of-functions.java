de9f488886038eceed00dc79fac86f1b60d59b3d
Archit Garg <garchit33@gmail.com>
2025-12-07 21:10:48 +0530

    Time: 21 ms (5.04%), Space: 47.2 MB (37.38%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        int prev = -1;
+        for(String str : logs){
+            String arr[] = str.split(":");
+            int id = Integer.parseInt(arr[0]);
+            int currTime = Integer.parseInt(arr[2]);
+            boolean isStart = arr[1].equals("start")?true:false;
+
+            if(isStart){
+                if(!stack.isEmpty()){
+                    int topId = stack.peek();
+                    ans[topId]+=currTime-prev-1;
+                }
+                stack.push(id);
+            }else {
+                int topId = stack.pop();
+                ans[topId]+=currTime-prev+1;
+            }
+
+            prev = currTime;
+        }
+        return ans;  
+    }
+}
