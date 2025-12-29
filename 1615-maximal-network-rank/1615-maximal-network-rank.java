b1b49f6c99ac7c199e0ebbd4c0c041d3e25f096a
Archit Garg <garchit33@gmail.com>
2025-12-29 20:36:16 +0530

    Time: 4 ms (98.14%), Space: 48.1 MB (7.44%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,21 +1,25 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        List<List<Integer>> graph = new ArrayList<>();
+        int[] degree = new int[n];
+        boolean[][] isConnected = new boolean[n][n];
 
-        for(int i=0; i<n; i++){
-            graph.add(new ArrayList<>());
+        for(int[] road : roads){
+            int u = road[0];
+            int v = road[1];
+
+            degree[u]++;
+            degree[v]++;
+
+            isConnected[u][v] = true;
+            isConnected[v][u] = true;
         }
 
-        for(int[] r : roads){
-            graph.get(r[0]).add(r[1]);
-            graph.get(r[1]).add(r[0]);
-        }
-
-        int max=0;
+        int max = 0;
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
-                int rank = graph.get(i).size() + graph.get(j).size();
-                if(graph.get(i).contains(j))
+                int rank = degree[i] + degree[j];
+
+                if(isConnected[i][j] && isConnected[j][i])
                     rank--;
 
                 max = Math.max(max, rank);
