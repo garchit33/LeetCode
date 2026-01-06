42cd55dabd08fdf422439ae0544bd89d8c93ea09
Archit Garg <garchit33@gmail.com>
2026-01-06 08:46:33 +0530

    Time: 224 ms (18.59%), Space: 84.1 MB (10.41%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,26 +1,22 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
         int mod = 1_000_000_007;
-        int[] arr = new int[n+1];
+        List<Integer> list = new ArrayList<>();
         for(int i=0; i<n; i++){
-            arr[i+1] = arr[i] + nums[i];
-        }
-
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b) -> a-b);
-
-        for(int i=1; i<=n; i++){
-            for(int j=0; j<i; j++){
-                pq.offer(arr[i]-arr[j]);
+            int sum = 0;
+            for(int j=i; j<n; j++){
+                sum+=nums[j];
+                list.add(sum);
             }
         }
 
-        long ans = 0;
-        for(int i=1; i<=right; i++){
-            int val = pq.poll();
-            if(i >= left){
-                ans = (ans + val) % mod;
-            }
+        Collections.sort(list);
+
+        int ans = 0;
+        for(int i=left-1; i<right; i++){
+            ans = (ans + list.get(i)) % mod;
         }
-        return (int) ans;
+
+        return ans;
     }
 }
