e496c74381d09a9d38198fbafa1513d3856a44a7
Archit Garg <garchit33@gmail.com>
2026-02-25 08:51:46 +0530

    Time: 2 ms (80.83%), Space: 46.9 MB (27.31%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -2,14 +2,17 @@ class Solution {
     List<List<String>> res = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
-        for(char[] ch : board)
-            Arrays.fill(ch,'.');
-        
-        makeBoard(board, 0);
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                board[i][j] = '.';
+            }
+        }
+
+        evaluvateBoard(board, 0);
         return res;
     }
 
-    private void makeBoard(char[][] board, int row){
+    private void evaluvateBoard(char[][] board, int row){
         if(row == board.length){
             List<String> list = new ArrayList<>();
             for(int i=0; i<board.length; i++){
@@ -18,37 +21,33 @@ class Solution {
             res.add(list);
         }
 
-        for(int col = 0; col < board.length; col++){
+        for(int col=0; col<board.length; col++){
             if(isSafe(board, row, col)){
                 board[row][col] = 'Q';
-                makeBoard(board, row+1);
+                evaluvateBoard(board, row+1);
                 board[row][col] = '.';
             }
         }
     }
 
     private boolean isSafe(char[][] board, int row, int col){
-        //Check top rows
         for(int i=0; i<row; i++){
             if(board[i][col] == 'Q')
                 return false;
         }
 
-        //Check left Diagonal
         int maxLeft = Math.min(row,col);
-        for(int i=1; i<=maxLeft; i++){
+        for(int i=0; i<=maxLeft; i++){
             if(board[row-i][col-i] == 'Q')
                 return false;
         }
 
-        //Check right Diagonal
         int maxRight = Math.min(row, board.length-1-col);
-        for(int i=1; i<=maxRight; i++){
+        for(int i=0; i<=maxRight; i++){
             if(board[row-i][col+i] == 'Q')
                 return false;
         }
+
         return true;
     }
-
-
 }
