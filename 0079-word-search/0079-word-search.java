3eb821048506c39f549affecdef59c36c32bc461
Archit Garg <garchit33@gmail.com>
2026-02-25 15:06:41 +0530

    Time: 134 ms (62.68%), Space: 43 MB (61.69%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean[][] vis = new boolean[board.length][board[0].length];
-
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(board[i][j] == word.charAt(0) && backtrack(board, word, vis, i, j, 0)) {
+                if(backtrack(board, word, i, j, 0)) {
                     return true;
                 }
             }
@@ -12,23 +10,24 @@ class Solution {
         return false;
     }
 
-    private boolean backtrack(char[][] board, String word, boolean[][] vis, int i, int j, int idx){
+    private boolean backtrack(char[][] board, String word, int i, int j, int idx){
         if(idx == word.length())
             return true;
         
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length || vis[i][j] || board[i][j] != word.charAt(idx))
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j] == '#' || board[i][j] != word.charAt(idx))
             return false;
         
-        vis[i][j] = true;
+        char curValue = board[i][j];
+        board[i][j] = '#';
         
-        if(backtrack(board, word, vis, i+1, j, idx+1)
-        || backtrack(board, word, vis, i-1, j, idx+1)
-        || backtrack(board, word, vis, i, j+1, idx+1)
-        || backtrack(board, word, vis, i, j-1, idx+1)){
+        if(backtrack(board, word, i+1, j, idx+1)
+        || backtrack(board, word, i-1, j, idx+1)
+        || backtrack(board, word, i, j+1, idx+1)
+        || backtrack(board, word, i, j-1, idx+1)){
             return true;
         }
 
-        vis[i][j] = false;
+        board[i][j] = curValue;
         return false;
     }
 }
