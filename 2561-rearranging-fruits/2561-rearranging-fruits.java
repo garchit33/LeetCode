bf0843db1999ff5541c3994d4c72d74150bb0f2e
Archit Garg <garchit33@gmail.com>
2026-03-10 00:50:35 +0530

    Time: 58 ms (45.02%), Space: 110 MB (11.44%) - LeetHub

--- 2561-rearranging-fruits/2561-rearranging-fruits.java
+++ 2561-rearranging-fruits/2561-rearranging-fruits.java
@@ -1,32 +1,28 @@
 class Solution {
     public long minCost(int[] basket1, int[] basket2) {
         HashMap<Integer, Integer> totalCounts = new HashMap<>();
-        for(int fruit : basket1)
+        HashMap<Integer, Integer> basket1Count = new HashMap<>();
+        for(int fruit : basket1){
             totalCounts.put(fruit, totalCounts.getOrDefault(fruit,0)+1);
-        
+            basket1Count.put(fruit, basket1Count.getOrDefault(fruit,0)+1);
+        }
         
         for(int fruit : basket2)
             totalCounts.put(fruit, totalCounts.getOrDefault(fruit,0)+1);
         
         int minValue = Integer.MAX_VALUE;
+        List<Long> fruitsToSwap = new ArrayList<>();
         for(Map.Entry<Integer, Integer> entry : totalCounts.entrySet()){
             if(entry.getValue() % 2 != 0)
                 return -1;
             minValue = Math.min(minValue, entry.getKey());
-        }
 
-        HashMap<Integer, Integer> basket1Count = new HashMap<>();
-        for(int fruit : basket1)
-            basket1Count.put(fruit, basket1Count.getOrDefault(fruit,0)+1);
-        
-        List<Long> fruitsToSwap = new ArrayList<>();
-        for(Map.Entry<Integer, Integer> entry : totalCounts.entrySet()){
             int fruit = entry.getKey();
             int diff = basket1Count.getOrDefault(fruit, 0) - (entry.getValue()/2);
             for(int i=0; i<Math.abs(diff); i++){
                 fruitsToSwap.add((long) fruit);
             }
-        }
+        }            
 
         Collections.sort(fruitsToSwap);
 
