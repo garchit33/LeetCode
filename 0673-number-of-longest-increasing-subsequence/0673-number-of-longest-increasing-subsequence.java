a7d187e0661c66984ff98a770b68f7e5d2519702
Archit Garg <garchit33@gmail.com>
2026-03-23 17:13:01 +0530

    Time: 23 ms (20.43%), Space: 45.6 MB (51.24%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -4,15 +4,15 @@ class Solution {
         Arrays.fill(len, 1);
         int[] count = new int[nums.length];
         Arrays.fill(count, 1);
-        
+
         int maxLen = 1;
-        for(int i=0; i < nums.length; i++){
-            for(int j=0; j < i; j++){
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<i; j++){
                 if(nums[i] > nums[j]){
                     if(len[j]+1 > len[i]){
                         len[i] = len[j]+1;
                         count[i] = count[j];
-                    } else if(len[j]+1 == len[i]){
+                    }else if(len[j]+1 == len[i]){
                         count[i] += count[j];
                     }
                 }
@@ -23,9 +23,10 @@ class Solution {
         int ans = 0;
         for(int i=0; i<nums.length; i++){
             if(len[i] == maxLen){
-                ans += count[i];
+                ans+=count[i];
             }
         }
+
         return ans;
     }
 }
