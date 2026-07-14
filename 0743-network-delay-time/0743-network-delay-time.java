940da32844a143e56b9d555e5bc019be2b379e07
Archit Garg <garchit33@gmail.com>
2026-07-14 22:39:42 +0530

    Time: 10 ms (81.84%), Space: 49.2 MB (42.82%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,40 +1,41 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         List<List<int[]>> graph = new ArrayList<>();
-        for(int i=0; i<=n; i++){
+        for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for(int edge[] : times){
+        for (int[] edge : times) {
             int u = edge[0];
             int v = edge[1];
-            int w = edge[2];
-            graph.get(u).add(new int[]{v, w});
+            int wt = edge[2];
+            graph.get(u).add(new int[] { v, wt });
         }
 
         int[] dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[k] = 0;
 
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        q.offer(new int[]{k, 0});
-        
-        while(q.size() > 0){
-            int[] rm = q.poll();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
+        pq.offer(new int[]{k,0});
+
+        while(pq.size() > 0) {
+            int[] rm = pq.poll();
             int src = rm[0];
             int wt = rm[1];
             for(int[] nbr : graph.get(src)){
                 if(wt + nbr[1] < dist[nbr[0]]){
                     dist[nbr[0]] = wt + nbr[1];
-                    q.offer(new int[]{nbr[0], dist[nbr[0]]});
+                    pq.offer(new int[]{nbr[0], dist[nbr[0]]});
                 }
             }
         }
 
         int ans = 0;
-        for(int i = 1; i<=n; i++){
+        for(int i=1; i<=n; i++){
             if(dist[i] == Integer.MAX_VALUE)
                 return -1;
+            
             ans = Math.max(ans, dist[i]);
         }
 
