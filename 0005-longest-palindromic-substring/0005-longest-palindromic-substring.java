1d65d35f4eec67ea887ec502ec78a1b8522ca81f
Archit Garg <garchit33@gmail.com>
2026-07-19 14:36:22 +0530

    Time: 2829 ms (5.03%), Space: 105.8 MB (5.38%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,16 +1,17 @@
 class Solution {
     public String longestPalindrome(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        for(int[] d : dp){
+            Arrays.fill(d, -1);
+        }
+
         int maxLen = Integer.MIN_VALUE;
         int sp = 0;
-        int[][] dp = new int[s.length()][s.length()];
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
-        }
 
         for(int i=0; i<s.length(); i++){
             for(int j=i; j<s.length(); j++){
-                if(find(s, i, j, dp) == 1){
-                    if(j-i+1 > maxLen){
+                if(find(i, j, s, dp) == 1){
+                    if(maxLen < j-i+1){
                         maxLen = j-i+1;
                         sp = i;
                     }
@@ -21,15 +22,16 @@ class Solution {
         return s.substring(sp, sp+maxLen);
     }
 
-    private int find(String s, int i, int j, int[][] dp){
+    private int find(int i, int j, String s, int[][] dp){
         if(i >= j)
             return 1;
-
-        if(dp[i][j] != -1)
+        
+        if(dp[i][j] != -1){
             return dp[i][j];
+        }
         
         if(s.charAt(i) == s.charAt(j)){
-            return find(s, i+1, j-1, dp);
+            return find(i+1, j-1, s, dp);
         }
 
         return dp[i][j] = 0;
