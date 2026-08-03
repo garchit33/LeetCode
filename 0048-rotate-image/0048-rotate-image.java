e28936903794ab28ed62b91592803f852b991002
Archit Garg <garchit33@gmail.com>
2026-08-03 21:35:56 +0530

    Time: 0 ms (100%), Space: 43.7 MB (53.89%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,9 +1,7 @@
 class Solution {
     public void rotate(int[][] matrix) {
         transposeMatrix(matrix);
-        // printMatrix(matrix);
         inverseMatrix(matrix);
-        // printMatrix(matrix);
     }
 
     private void transposeMatrix(int[][] matrix){
@@ -24,15 +22,6 @@ class Solution {
                 matrix[i][j] = matrix[i][x];
                 matrix[i][x--] = temp;
             }
-        }
-    }
-
-    private void printMatrix(int[][] matrix){
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix.length; j++){
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
         }
     }
 }
