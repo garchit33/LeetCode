ef35ad399ff720a37f1a8e7623527a8e67cf5ed3
Archit Garg <garchit33@gmail.com>
2026-08-18 22:02:37 +0530

    Time: 131 ms (66.78%), Space: 42.8 MB (75.78%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(backtrack(board, word, i, j, 0)) {
+                if(find(board, word, i, j, 0)){
                     return true;
                 }
             }
@@ -10,24 +10,23 @@ class Solution {
         return false;
     }
 
-    private boolean backtrack(char[][] board, String word, int i, int j, int idx){
+    private boolean find(char[][] board, String word, int i, int j, int idx){
         if(idx == word.length())
             return true;
         
         if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j] == '#' || board[i][j] != word.charAt(idx))
             return false;
         
-        char curValue = board[i][j];
+        char currIdx = board[i][j];
         board[i][j] = '#';
-        
-        if(backtrack(board, word, i+1, j, idx+1)
-        || backtrack(board, word, i-1, j, idx+1)
-        || backtrack(board, word, i, j+1, idx+1)
-        || backtrack(board, word, i, j-1, idx+1)){
-            return true;
-        }
 
-        board[i][j] = curValue;
+        if(find(board,word,i+1,j,idx+1) || find(board,word,i-1,j,idx+1) 
+        || find(board,word,i,j+1,idx+1) || find(board,word,i,j-1,idx+1)){
+             return true;
+        }
+           
+        
+        board[i][j] = currIdx;
         return false;
     }
 }
