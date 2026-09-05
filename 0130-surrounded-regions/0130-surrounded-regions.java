c0d10fe2ef9dbc2124f7255b0420ec8e50da5f50
Archit Garg <garchit33@gmail.com>
2026-09-05 23:55:50 +0530

    Time: 5 ms (9.46%), Space: 47.4 MB (96.03%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -3,26 +3,28 @@ class Solution {
         Queue<int[]> q = new LinkedList<>();
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(i == 0 || j == 0 || i==board.length-1 || j==board[0].length-1){
-                    if(board[i][j] == 'O')
+                if(i==0 || j==0 || i == board.length-1 || j==board[0].length-1){
+                    if(board[i][j] == 'O'){
                         q.offer(new int[]{i,j});
+                    }
                 }
             }
         }
 
-        int[][] dirs = new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
+        int[][] dirs = new int[][]{{0,1}, {0,-1}, {1,0}, {-1,0}};
         while(q.size() > 0){
-            int[] rm  = q.poll();
+            int[] rm = q.poll();
             int x = rm[0];
             int y = rm[1];
-            board[x][y]='y';
+            board[x][y] = 'y';
             for(int[] dir : dirs){
                 int nx = x+dir[0];
                 int ny = y+dir[1];
-                if(checkValid(board, nx, ny)){
+                if(checkValid(nx, ny, board)){
                     q.offer(new int[]{nx,ny});
                 }
             }
+
         }
 
         for(int i=0; i<board.length; i++){
@@ -36,10 +38,10 @@ class Solution {
         }
     }
 
-    private boolean checkValid(char[][] board, int i, int j){
+    private boolean checkValid(int i, int j, char[][] board){
         if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j] == 'X' || board[i][j] == 'y')
             return false;
-
+        
         return true;
     }
 }
