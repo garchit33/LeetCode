31f1f19152fd7b88484f8d022abc52c8eecd260a
Archit Garg <garchit33@gmail.com>
2026-09-09 00:32:29 +0530

    Time: 60 ms (76.17%), Space: 84.9 MB (10.72%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -9,7 +9,7 @@ class Solution {
         }
     }
 
-    private void insert(TrieNode root, String word) {
+    private void insert(TrieNode root, String word){
         TrieNode curr = root;
         for(int i=0; i<word.length(); i++){
             int idx = word.charAt(i)-'a';
@@ -19,23 +19,24 @@ class Solution {
         }
         curr.isEnd = true;
     }
-    
-    private boolean dfs(TrieNode root, String word, int idx, Boolean[] dp){
+
+    private boolean find(TrieNode root, String word, int idx, Boolean[] dp){
         if(idx == word.length())
             return true;
         
         if(dp[idx] != null)
             return dp[idx];
-
+        
         TrieNode curr = root;
-        for(int i = idx; i < word.length(); i++){
-            int c = word.charAt(i)-'a';
-            if(curr.arr[c] == null)
+        for(int i=idx; i<word.length(); i++){
+            int x = word.charAt(i) - 'a';
+            if(curr.arr[x] == null)
                 break;
-            curr = curr.arr[c];
+            curr = curr.arr[x];
             if(curr.isEnd){
-                if(dfs(root, word, i+1, dp))
+                if(find(root, word, i+1, dp)){
                     return dp[idx] = true;
+                }
             }
         }
         return dp[idx] = false;
@@ -43,22 +44,20 @@ class Solution {
 
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         Arrays.sort(words, (a,b) -> a.length() - b.length());
-        TrieNode root = new TrieNode();
-        
         List<String> ans = new ArrayList<>();
 
-        for(int i=0; i<words.length; i++){
+        TrieNode root = new TrieNode();
 
+        for(int i=0; i<words.length; i++){
             if(words[i].length() == 0)
                 continue;
-
+            
             Boolean[] dp = new Boolean[words[i].length()];
-            if(dfs(root, words[i], 0, dp))
+            if(find(root, words[i], 0, dp))
                 ans.add(words[i]);
             
             insert(root, words[i]);
         }
-
         return ans;
     }
 }
